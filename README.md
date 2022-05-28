2302c45700784596f97f65d03196bb7fe22cd0a2
Anton Bessonov <anton.bessonov.public@gmail.com>
2022-05-28 13:36:39 +0200

    switch to esm

---
+++ README.md
@@ -0,0 +1,12 @@
+# Description
+
+Reproduction of https://github.com/microsoft/playwright/issues/14459
+
+## Instructions
+
+```bash
+npm install
+npx playwright install
+npm test
+npm run trace
+```
--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -95,7 +95,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
